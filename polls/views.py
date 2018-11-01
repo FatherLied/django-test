052de43096f1be86d3f1c6d9b4fae6be7535b73e
Tilmo8 <santino_gaara@yahoo.com>
2018-11-01 23:24:35 +0800

    Add View: polls/index
    Installed python debugger.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # pdb.set_trace()   # BreakPoint
+    return HttpResponse("Hello, world. You're at the polls index.")
